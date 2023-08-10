08c6641e71feaa7065939e6502f4c3d38f5f6b97
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-08-10 23:54:27 +0300

    feat: graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: { dateScalarMode: 'timestamp' },
+    }),
+  ],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
