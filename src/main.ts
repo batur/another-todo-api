8e8220cea1f8c86021b81f888783b9823d498e99
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-08-13 15:38:39 +0300

    fix: helmet

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,26 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.enableCors();
-
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
